f4bab4c6dc3d86dd95fae289f99e8c3a9cc81de0
[Nuh Genc] <[nuhgnc1@gmail.com]>
2022-02-15 13:54:04 +0300

    otomatik çalıştırma eklendi

--- patika.dev/odev/main.js
+++ patika.dev/odev/main.js
@@ -7,8 +7,8 @@ function clickedBtn (){
   userInfo.innerHTML = userName
   
   let d = new Date()
-  let dateoptions = { day:'2-digit',month:'2-digit',year:'numeric', weekday:'long', hour:'numeric',minute:'numeric'}
+  let dateoptions = { day:'2-digit',month:'2-digit',year:'numeric', weekday:'long', hour:'numeric',minute:'numeric', second:'numeric'}
   
    date.innerHTML = d.toLocaleDateString("tr-TR",dateoptions)
-  
+   setInterval(clickedBtn, 1000); 
 }
